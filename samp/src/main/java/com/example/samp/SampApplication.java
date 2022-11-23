7974a109f1856276ccb4b157901b429b9ccfac32
SARAN KUMAR D <64687455+saranbhai007@users.noreply.github.com>
2022-11-23 19:42:42 +0530

    Add files via upload

---
+++ samp/src/main/java/com/example/samp/SampApplication.java
@@ -0,0 +1,13 @@
+package com.example.samp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampApplication.class, args);
+	}
+
+}
---
+++ samp/src/test/java/com/example/samp/SampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
